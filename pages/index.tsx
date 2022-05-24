677a5fc15bc90a5e21d7e42fcb4ea24bb131e218
Sennay <wsennay@gmail.com>
2022-05-24 16:37:49 +0300

    add info

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,22 @@ const Home: NextPage = () => {
   const {data:session} = useSession()
 
   if(session) {
-    return <>
-     token: {session.role}
+    return <div>
+      <p>email:{session.email}</p>
+      <p>role:{session.role}</p>
       <button onClick={()=>signOut()} >Sign out</button>
       <button onClick={()=>{
       console.log(session)
       }
       } >print</button>
-    </>
+    </div>
   }
 
   return  <>
-  Not Signed In
+  <p>Not Signed In</p>
+     <p> You can use any user credentials in the heroku</p>
+     <p> For example: email: wsennay@gmail.com</p>
+     <p> password: 12345678</p>
     <button onClick={()=>signIn("credentials")} >Sign in</button>
   </>
 
